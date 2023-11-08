52c3a81510a8f566d87d950699ea8bde598b6469
nekosecur <150216404+nekosecur@users.noreply.github.com>
2023-11-09 02:54:26 +0700

    Update member_handler.py

--- plugins/command/member_handler.py
+++ plugins/command/member_handler.py
@@ -1,94 +1,106 @@
 import re
-from pyrogram import Client, types, enums
-from plugins import Database
-import config  # Pastikan untuk mengimpor konfigurasi Anda
 
-async def member_handler(client: Client, msg: types.Message):
-    db = Database(msg.from_user.id)
+from pyrogram import Client, enums, types
+from plugins import Database, Helper
 
-    if re.search(r"^[\/]addmember(\s|\n)*$", msg.text or msg.caption):
-        # Hanya izinkan admin
-        if msg.from_user.id != config.id_admin:
-            return
-
-        return await msg.reply_text(
-            text="<b>Cara penggunaan tambah member</b>\n\n<code>/addmember id_user</code>\n\nContoh :\n<code>/addmember 121212021</code>",
-            quote=True,
-            parse_mode=enums.ParseMode.HTML
-        )
-
-    if re.search(r"^[\/]hapusmember(\s|\n)*$", msg.text or msg.caption):
-        # Hanya izinkan admin
-        if msg.from_user.id != config.id_admin:
-            return
-
-        return await msg.reply_text(
-            text="<b>Cara penggunaan hapus member</b>\n\n<code>/hapusmember id_user</code>\n\nContoh :\n<code>/hapusmember 121212021</code>",
-            quote=True,
-            parse_mode=enums.ParseMode.HTML
-        )
-
-async def tambah_member_handler(client: Client, msg: types.Message):
+async def tambah_anggota_handler(client: Client, msg: types.Message):
     helper = Helper(client, msg)
-
-    if not (y := re.search(r"^[\/]addmember(\s|\n)*(\d+)$", msg.text or msg.caption)):
+    if re.search(r"^[\/]addmember(\s|\n)*$", msg.text or msg.caption):
         return await msg.reply_text(
-            text="<b>Cara penggunaan tambah member</b>\n\n<code>/addmember id_user</code>\n\nContoh :\n<code>/addmember 121212021</code>",
-            quote=True,
-            parse_mode=enums.ParseMode.HTML
+            text="<b>Cara penggunaan tambah anggota</b>\n\n<code>/addmember id_user</code>\n\nContoh :\n<code>/addmember 121212021</code>",
+            quote=True, parse_mode=enums.ParseMode.HTML
         )
-
-    target = int(y[2])
-    db = Database(target)
-
-    # Hanya izinkan admin
-    if msg.from_user.id != config.id_admin:
-        return
-
-    # Periksa apakah pengguna diblokir atau tidak terdaftar di database
-    if target in db.get_data_bot(client.id_bot).ban:
+    if not (
+        y := re.search(
+            r"^[\/]addmember(\s|\n)*(\d+)$", msg.text or msg.caption
+        )
+    ):
         return await msg.reply_text(
-            text=f"<i><a href='tg://user?id={str(target)}'>User</a> sedang dalam kondisi banned</i>\n└Tidak dapat menjadi member",
-            quote=True,
-            parse_mode=enums.ParseMode.HTML
+            text="<b>Cara penggunaan tambah anggota</b>\n\n<code>/addmember id_user</code>\n\nContoh :\n<code>/addmember 121212021</code>",
+            quote=True, parse_mode=enums.ParseMode.HTML
         )
-
+    target = y[2]
+    db = Database(int(target))
     if not await db.cek_user_didatabase():
         return await msg.reply_text(
             text=f"<i><a href='tg://user?id={str(target)}'>User</a> tidak terdaftar di database</i>",
-            quote=True,
-            parse_mode=enums.ParseMode.HTML
+            quote=True, parse_mode=enums.ParseMode.HTML
         )
 
-    # Periksa status pengguna
     status = [
         'admin', 'owner', 'talent', 'daddy sugar', 'moans girl',
         'moans boy', 'girlfriend rent', 'boyfriend rent'
     ]
     member = db.get_data_pelanggan()
-
     if member.status in status:
         return await msg.reply_text(
-            text=f"❌<i>Terjadi kesalahan, <a href='tg://user?id={str(target)}'>user</a> adalah seorang {member.status.upper()} tidak dapat ditambahkan menjadi member</i>",
-            quote=True,
-            parse_mode=enums.ParseMode.HTML
+            text=f"❌<i>Terjadi kesalahan, <a href='tg://user?id={str(target)}'>user</a> adalah seorang {member.status.upper()} dan tidak dapat ditambahkan sebagai anggota</i>",
+            quote=True, parse_mode=enums.ParseMode.HTML
         )
 
     try:
-        a = await client.get_users(target)
+        a = await client.get_chat(target)
         nama = await helper.escapeHTML(
             f'{a.first_name} {a.last_name}' if a.last_name else a.first_name
         )
-
-        await db.addmember(target)
-        return await msg.reply_text(
-            text=f"<a href='tg://openmessage?user_id={str(target)}'>User</a> <i>berhasil menjadi member</i>",
-            quote=True,
+        await client.send_message(
+            int(target),
+            text=f"<i>Kamu telah ditambahkan sebagai anggota bot</i>\n└Ditambahkan oleh : <a href='tg://openmessage?user_id={str(client.id_bot)}'>Bot</a>",
             parse_mode=enums.ParseMode.HTML
+        )
+        await db.tambah_anggota(int(target), client.id_bot, nama)
+        return await msg.reply_text(
+            text=f"<a href='tg://openmessage?user_id={str(target)}'>User</a> <i>berhasil ditambahkan sebagai anggota bot</i>\n└Ditambahkan oleh : <a href='tg://openmessage?user_id={str(client.id_bot)}'>Bot</a>",
+            quote=True, parse_mode=enums.ParseMode.HTML
         )
     except Exception as e:
         return await msg.reply_text(
             text=f"❌<i>Terjadi kesalahan, sepertinya user memblokir bot</i>\n\n{e}",
-            quote=True,
+            quote=True, parse_mode=enums.ParseMode.HTML
+       )
+
+async def hapus_anggota_handler(client: Client, msg: types.Message):
+    if re.search(r"^[\/]hapusmember(\s|\n)*$", msg.text or msg.caption):
+        return await msg.reply_text(
+            text="<b>Cara penggunaan hapus anggota</b>\n\n<code>/hapusmember id_user</code>\n\nContoh :\n<code>/hapusmember 121212021</code>",
+            quote=True, parse_mode=enums.ParseMode.HTML
+        )
+    if not (
+        x := re.search(
+            r"^[\/]hapusmember(\s|\n)*(\d+)$", msg.text or msg.caption
+        )
+    ):
+        return await msg.reply_text(
+            text="<b>Cara penggunaan hapus anggota</b>\n\n<code>/hapusmember id_user</code>\n\nContoh :\n<code>/hapusmember 121212021</code>",
+            quote=True, parse_mode=enums.ParseMode.HTML
+        )
+    target = x[2]
+    db = Database(int(target))
+    if not await db.cek_user_didatabase():
+        return await msg.reply_text(
+            text=f"<i><a href='tg://user?id={str(target)}'>User</a> tidak terdaftar di database</i>",
+            quote=True, parse_mode=enums.ParseMode.HTML
+        )
+    member = db.get_data_pelanggan()
+    if member.status in ['owner', 'admin']:
+        return await msg.reply_text(
+            text=f"❌<i>Terjadi kesalahan, <a href='tg://user?id={str(target)}'>user</a> adalah seorang {member.status.upper()} dan tidak dapat dihapus sebagai anggota</i>",
+            quote=True, parse_mode=enums.ParseMode.HTML
+        )
+
+    try:
+        await client.send_message(
+            int(target),
+            text=f"<i>Maaf, kamu telah dihapus dari anggota bot</i>\n└Dihapus oleh : <a href='tg://openmessage?user_id={str(client.id_bot)}'>Bot</a>",
             parse_mode=enums.ParseMode.HTML
+        )
+        await db.hapus_anggota(int(target), client.id_bot)
+        return await msg.reply_text(
+            text=f"<a href='tg://openmessage?user_id={str(target)}'>User</a> <i>berhasil dihapus sebagai anggota bot</i>\n└Dihapus oleh : <a href='tg://openmessage?user_id={str(client.id_bot)}'>Bot</a>",
+            quote=True, parse_mode=enums.ParseMode.HTML
+        )
+    except Exception as e:
+        return await msg.reply_text(
+            text=f"❌<i>Terjadi kesalahan, sepertinya user memblokir bot</i>\n\n{e}",
+            quote=True, parse_mode=enums.ParseMode.HTML
         )
